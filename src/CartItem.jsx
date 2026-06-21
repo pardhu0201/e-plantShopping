0cac1fcd659ed3df4328e8178fb1d909ed9da001
Bathula Pardhu <pardhu.b23@iiits.in>
2026-06-21 22:01:23 +0530

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,51 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart
+      .reduce(
+        (total, item) =>
+          total +
+          parseFloat(item.cost.substring(1)) * item.quantity,
+        0
+      )
+      .toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(
+      updateQuantity({
+        name: item.name,
+        amount: item.quantity + 1,
+      })
+    );
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(
+        updateQuantity({
+          name: item.name,
+          amount: item.quantity - 1,
+        })
+      );
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
-
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return (
+      parseFloat(item.cost.substring(1)) * item.quantity).toFixed(2);
   };
 
   return (
